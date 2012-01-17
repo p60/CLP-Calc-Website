6d2657e6dabd14291172f79af7d40c8ad35561ef
Paul Navasard <pnavasard@gmail.com>
2012-01-17 09:47:50 -0700

    Test rewrite

--- spec/slider-test.js
+++ spec/slider-test.js
@@ -5,7 +5,12 @@ describe("The Slider", function() {
   before(function() {
     $('body').append('<div class="slider"><span class="min">25</span><span class="max">1000</span><span style="display: block; padding: 0; margin: 0;" class="value"><span>0</span><span></span></span><div></div></div>');
     this.slider = new CLPSlider({
-      element: $('.slider')
+      min: 25,
+      max: 75
+    });
+    this.sliderView = new SliderUI({
+      el: $('.slider'),
+      model: this.slider
     });
   });
 
@@ -14,19 +19,25 @@ describe("The Slider", function() {
   });
 
   it("should create a slider", function() {
-    expect(this.slider.control.slider('value')).toEqual(25);
+    expect(this.slider.get('value')).toEqual(25);
+    expect(this.sliderView.control.slider('value')).toEqual(25);
   });
 
-  it("should set the max and min of the slider based on the html", function() {
-    min = parseInt($('.min').text());
-    max = parseInt($('.max').text());
-    expect(this.slider.control.slider('option', 'min')).toEqual(min);
-    expect(this.slider.control.slider('option', 'max')).toEqual(max);
+  it("should set the max and min of the slider based on the options", function() {
+    expect(this.sliderView.control.slider('option', 'min')).toEqual(25);
+    expect(this.sliderView.control.slider('option', 'max')).toEqual(75);
+    expect(this.slider.get('min')).toEqual(25);
+    expect(this.slider.get('max')).toEqual(75);
   });
 
-  it("should set the value of the value span to the value of the slider when it is changed", function() {
-    this.slider.control.slider('value', 50);
-    expect($('.value span').first().text()).toEqual('50');
+  it("should set the value of the model when the view is changed", function() {
+    this.sliderView.control.slider('value', 50);
     expect(this.slider.get('value')).toEqual(50);
+  });
+
+  it("should return the correct percentage for values", function() {
+    expect(this.slider.percentage()).toEqual(-6);
+    this.sliderView.control.slider('value', 50);
+    expect(this.slider.percentage()).toEqual(44);
   });
 });
